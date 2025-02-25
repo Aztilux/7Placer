caf3409f0e5d68e34ef65be6756bd9664acd93ed
Azti <aztilux@gmail.com>
2025-02-25 16:30:32 +0100

    Removed useless export

--- src/variables.ts
+++ src/variables.ts
@@ -1,18 +1,18 @@
 import * as cmd from './auth/util/commands'
 
-export default (window as any).seven = {
-  bots: [],
-  pixelspeed: 21,
-  queue: [],
-  inprogress: false,
-  protect: false,
-  tickspeed: 1000,
-  order: 'fromCenter',
-  saveAuth: cmd.saveAuth,
-  getAuth: cmd.getAuth,
-  saveAccount: cmd.saveAccount,
-  getAccounts: cmd.getAccounts,
-  deleteAccount: cmd.deleteAccount,
-  connect: cmd.connect,
-  disconnect: cmd.disconnect,
+(window as any).seven = {
+    bots: [],
+    pixelspeed: 21,
+    queue: [],
+    inprogress: false,
+    protect: false,
+    tickspeed: 1000,
+    order: 'fromCenter',
+    saveAuth: cmd.saveAuth,
+    getAuth: cmd.getAuth,
+    saveAccount: cmd.saveAccount,
+    getAccounts: cmd.getAccounts,
+    deleteAccount: cmd.deleteAccount,
+    connect: cmd.connect,
+    disconnect: cmd.disconnect,
 }
