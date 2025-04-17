dfd90b6236d08fd91c1e4dcf7fadbf9a96ccbfce
Azti <aztilux@gmail.com>
2025-04-17 22:17:11 +0200

    (add) Settings tab

---
+++ src/GUI/TAB_Settings.ts
@@ -0,0 +1,28 @@
+import { MainGUI } from "./GUICore"
+import packageInfo from '../../package.json';
+
+$(function() {
+    const gui = MainGUI.instance
+    const tab = gui.getTab("settings")
+
+    const bot_settings = tab.createSubmenu("bot settings")
+    bot_settings.createText("pixel speed default is 21")
+    const pixelspeed_input = bot_settings.createInput(`pixel speed`, "number", (number) => {
+        number = parseInt(number)
+        if (number < 16) number = 16
+        window.seven.pixelspeed = number
+    })
+
+    const sort_options = [
+        {label: "random", value: "rand"},
+        {label: "colors", value: "colors"},
+        {label: "vertical", value: "vertical"},
+        {label: "horizontal", value: "horizontal"},
+        {label: "circle", value: "circle"},
+    ]
+    bot_settings.createSelect('-- select sorting --', sort_options, value => {
+        window.seven.order = value
+    })
+
+    tab.createSubmenu("version").createText("7Placer version " + packageInfo.version)
+})
