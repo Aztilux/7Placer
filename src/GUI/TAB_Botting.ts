b3767036cc352a8a209c6f338b6118732753f34f
Azti <aztilux@gmail.com>
2025-04-17 22:17:27 +0200

    (add) Squares and dithering to botting tab

--- src/GUI/TAB_Botting.ts
+++ src/GUI/TAB_Botting.ts
@@ -1,33 +1,80 @@
-import { botImage } from "../modules";
+import { botImage, BotSquare } from "../modules";
 import Queue from "../modules/defaultModules/Queue";
+import getClientMouse from "../modules/util/getClientMouse";
 import { MainGUI } from "./GUICore";
 
 $(function() {
     const GUI = MainGUI.instance;
     const TAB = GUI.createTab("Botting", "https://pngimg.com/d/android_logo_PNG5.png");
-    const image_submenu = TAB.createSubmenu("Images");
 
+    const image_submenu = TAB.createSubmenu("Images");
     let current_image: File;
-    let current_x: number;
-    let current_y: number;
+    let current_image_x: number;
+    let current_image_y: number;
     image_submenu.createDrop("Drop Image", dropped_image => {
         current_image = dropped_image;
-    })
+    });
     image_submenu.createInput("X", "number", x_coord => {
-        current_x = parseInt(x_coord);
-    })
+        current_image_x = parseInt(x_coord);
+    });
     image_submenu.createInput("Y", "number", y_coord => {
-        current_y = parseInt(y_coord);
+        current_image_y = parseInt(y_coord);
+    });
+
+    const dithering_options = [
+        {label: "None", value: "None"},
+        {label: "FloydSteinberg", value: "FloydSteinberg"},
+        {label: "FalseFloydSteinberg", value: "FalseFloydSteinberg"},
+        {label: "Stucki", value: "Stucki"},
+        {label: "Atkinson", value: "Atkinson"},
+        {label: "Jarvis", value: "Jarvis"},
+        {label: "Burkes", value: "Burkes"},
+        {label: "Sierra", value: "Sierra"},
+        {label: "TwoSierra", value: "TwoSierra"},
+        {label: "SierraLite", value: "SierraLite"},
+    ]
+    image_submenu.createSelect("-- select dithering --", dithering_options, (value) => {
+        if (value == "None") {
+            window.seven.dither = null
+            return
+        }
+        window.seven.dither = value
     })
+
     image_submenu.createButton("Start", () => {
-        botImage(current_x, current_y, current_image)
-    })
+        botImage(current_image_x, current_image_y, current_image);
+    });
     image_submenu.createButton("Stop", () => {
-        Queue.stop()
-    })
+        Queue.stop();
+    });
 
     const protecting_submenu = TAB.createSubmenu("Protecting");
     protecting_submenu.createToggle("Protect", false, (state) => {
-        window.seven.protect = state
-    })
+        window.seven.protect = state;
+    });
+
+    const square_submenu = TAB.createSubmenu("Squares");
+    let square_x1: number;
+    let square_y1: number;
+    let square_x2: number;
+    let square_y2: number;
+    square_submenu.createText("Select color as usual")
+    square_submenu.createInput('X1', "number", text => {
+        square_x1 = parseInt(text);
+    });
+    square_submenu.createInput('Y1', "number", text => {
+        square_y1 = parseInt(text);
+    });
+    square_submenu.createInput('X2', "number", text => {
+        square_x2 = parseInt(text);
+    });
+    square_submenu.createInput('Y2', "number", text => {
+        square_y2 = parseInt(text);
+    });
+    square_submenu.createButton("Start", () => {
+        BotSquare(square_x1, square_y1, square_x2, square_y2, getClientMouse()[2]);
+    });
+    square_submenu.createButton("Stop", () => {
+        Queue.stop();
+    });
 })
