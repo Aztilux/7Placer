30b9dde4454d816fa5fb7e8f401423ed8b541720
Azti <aztilux@gmail.com>
2025-04-17 19:27:40 +0200

    (modify) ImageProcessing now uses webworkers

--- src/modules/defaultModules/ImageTools.ts
+++ src/modules/defaultModules/ImageTools.ts
@@ -15,13 +15,12 @@ function getColorDistance(c1: {r: number, g: number, b: number}, c2: [r: number,
     return (c1.r - c2[0]) ** 2 + (c1.g - c2[1]) ** 2 + (c1.b - c2[2]) ** 2;
 }
 
-function findClosestColor(color: {r: number, g: number, b: number}) {
-    const canvas = Canvas.instance
+function findClosestColor(color: {r: number, g: number, b: number}, palette: [r: number, g: number, b:number][]) {
     let minDistance = Infinity;
     let colorNumber: number
     let index = 0
-    for (const pxpColor of canvas.colors) {
-        const distance = getColorDistance(color, pxpColor);
+    for (const palette_color of palette) {
+        const distance = getColorDistance(color, palette_color);
         if (distance < minDistance) {
         minDistance = distance;
         colorNumber = index
@@ -41,8 +40,83 @@ function previewCanvasImage (x: number, y: number, image: File) {
     img.src = URL.createObjectURL(image);
 }
 
-export async function ImageToPixels(image: ImageBitmap, dither?: string) { // dither options: FloydSteinberg, FalseFloydSteinberg, Stucki, Atkinson, Jarvis, Burkes, Sierra, TwoSierra, SierraLite
-    const result = [];
+const workerCode = `
+    function getColorDistance(c1, c2) {
+        return (c1.r - c2[0]) ** 2 + (c1.g - c2[1]) ** 2 + (c1.b - c2[2]) ** 2;
+    }
+    function findClosestColor(color, palette) {
+        let minDistance = Infinity;
+        let colorNumber
+        let index = 0
+        for (const palette_color of palette) {
+            const distance = getColorDistance(color, palette_color);
+            if (distance < minDistance) {
+            minDistance = distance;
+            colorNumber = index
+            }
+            index += 1
+        }
+        return colorNumber;
+    }
+
+    self.onmessage = function (e) { // {imageData: ImageData, palette: [r, g, b][],  y_division_last: number, x_division_last: number}
+        const pixelData = e.data.imageData.data
+        const result = []
+        for (let y = 0; y < e.data.y_division_last; y++) {
+            for (let x = 0; x < e.data.x_division_last; x++) {
+                const pixelIndex = (y * e.data.imageData.width + x) * 4;
+                const r = pixelData[pixelIndex];
+                const g = pixelData[pixelIndex + 1];
+                const b = pixelData[pixelIndex + 2];
+                const a = pixelData[pixelIndex + 3];
+                if (a < 1) {
+                    continue; // ignore transparent pixels
+                };
+                const color = findClosestColor({r, g, b}, e.data.palette);
+                result.push({x, y, color});
+            };
+        }
+        self.postMessage(result);
+    };
+`;
+const blob = new Blob([workerCode], { type: 'application/javascript' });
+const blobUrl = URL.createObjectURL(blob);
+
+function imageData2array(imageData: ImageData, thread_amount: number, palette: [r: number, g: number, b: number][]) {
+    const t0 = performance.now();
+    return new Promise(resolve => {
+        const workers_global_result: {x: number, y: number, color: number}[] = [];
+        let workers = 0
+        let workers_done = 0
+        for (let divison_y = 0; divison_y < imageData.height; divison_y += (imageData.height / thread_amount)) {
+            const y_division_last = divison_y + (imageData.height / thread_amount)
+            for (let division_x = 0; division_x < imageData.width; division_x += (imageData.width / thread_amount)) {
+                const x_division_last = division_x + (imageData.width / thread_amount)
+                const worker = new Worker(blobUrl);
+                workers += 1
+                worker.postMessage({imageData: imageData, palette: palette, y_division_last: y_division_last, x_division_last: x_division_last});
+                worker.onmessage = function (e) {
+                    const worker_pixels = e.data
+                    worker.terminate();
+                    workers_done += 1
+                    console.log(workers_done)
+                    worker_pixels.forEach((pixel: {x: number, y: number, color: number}) => {
+                        workers_global_result.push(pixel)
+                    });
+                    if (workers == workers_done) {
+                        console.log(`ImageData processed in ${performance.now() - t0} ms`)
+                        resolve(workers_global_result)
+                    }
+                }
+            }
+        }
+    })
+    .then((final_result: {x: number, y: number, color: number}[]) => {
+        return final_result
+    })
+}
+
+export async function ImageToPixels(image: ImageBitmap, dither?: string, palette?: [r: number, g: number, b: number][]) { // dither options: FloydSteinberg, FalseFloydSteinberg, Stucki, Atkinson, Jarvis, Burkes, Sierra, TwoSierra, SierraLite
     const canvas = new OffscreenCanvas(image.width, image.height);
     const ctx = canvas.getContext('2d');
     ctx.drawImage(image, 0, 0, image.width, image.height);
@@ -52,24 +126,8 @@ export async function ImageToPixels(image: ImageBitmap, dither?: string) { // di
         quant.sample(imageData);
         quant.reduce(imageData, 1, dither)
     }
-    const pixelData = imageData.data;
 
-    for (let y = 0; y < canvas.height; y++) {
-        for (let x = 0; x < canvas.width; x++) {
-        const pixelIndex = (y * canvas.width + x) * 4;
-        const r = pixelData[pixelIndex];
-        const g = pixelData[pixelIndex + 1];
-        const b = pixelData[pixelIndex + 2];
-        const a = pixelData[pixelIndex + 3];
-        if (a < 1) {
-            continue; // ignore transparent pixels
-        };
-        const color = findClosestColor({r, g, b});
-        result.push({x, y, color});
-        };
-    }
-
-    return result;
+    return imageData2array(imageData, 2, palette || Canvas.instance.colors);
 }
 
 export async function botImage(x: number, y: number, image: File) {
