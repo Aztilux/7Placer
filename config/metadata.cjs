e2a3766821f547716fe98394e31060127a16b924
Azti <aztilux@gmail.com>
2025-04-18 02:10:11 +0200

    (modify) jquery import to cdnjs

--- config/metadata.cjs
+++ config/metadata.cjs
@@ -11,9 +11,8 @@ module.exports = {
   author: author,
   include: '/^https:\\/\\/pixelplace.io\\/\\d+-/',
   require: [
+    `https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js`,
     `https://update.greasyfork.org/scripts/498080/1395134/Hacktimer.js`,
-    `https://pixelplace.io/js/jquery.min.js?v2=1`,
-    `https://pixelplace.io/js/jquery-ui.min.js?v2=1`,
     `https://cdnjs.cloudflare.com/ajax/libs/toastify-js/1.12.0/toastify.js`,
     `https://cdn.jsdelivr.net/npm/rgbquant@1.1.2/src/rgbquant.min.js`,
   ],
