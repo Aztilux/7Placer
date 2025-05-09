c00b60b9b141d7f33124e47fb418e74d44f2f183
Azti <aztilux@gmail.com>
2025-05-09 17:43:29 +0200

    Renamed build file name

--- config/webpack.config.prod.cjs
+++ config/webpack.config.prod.cjs
@@ -9,7 +9,7 @@ const webpackConfig = require("./webpack.config.base.cjs");
 const cfg = merge(webpackConfig, {
   mode: "production",
   output: {
-    filename: "index.prod.user.js",
+    filename: "script.user.js",
   },
   optimization: {
     // if you need minimize, you need to config minimizer to keep all comments
