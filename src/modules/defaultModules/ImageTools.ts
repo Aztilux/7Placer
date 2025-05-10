9e639fa5867657aeed98656b5ee4d555afc2046c
Azti <aztilux@gmail.com>
2025-05-10 18:51:23 +0200

    (remove) multi workers (only one now)

--- src/modules/defaultModules/ImageTools.ts
+++ src/modules/defaultModules/ImageTools.ts
@@ -11,25 +11,6 @@ export function hex2rgb(hex: string): [r: number, g: number, b: number] {
   return [ r, g, b ];
 }
 
-// function getColorDistance(c1: {r: number, g: number, b: number}, c2: [r: number, g: number, b: number]) {
-//     return (c1.r - c2[0]) ** 2 + (c1.g - c2[1]) ** 2 + (c1.b - c2[2]) ** 2;
-// }
-
-// function findClosestColor(color: {r: number, g: number, b: number}, palette: [r: number, g: number, b:number][]) {
-//     let minDistance = Infinity;
-//     let colorNumber: number
-//     let index = 0
-//     for (const palette_color of palette) {
-//         const distance = getColorDistance(color, palette_color);
-//         if (distance < minDistance) {
-//         minDistance = distance;
-//         colorNumber = index
-//         }
-//         index += 1
-//     }
-//     return colorNumber;
-// }
-
 function previewCanvasImage (x: number, y: number, image: File) {
     const canvas = Canvas.instance;
     const ctx = canvas.previewCanvas;
@@ -59,11 +40,11 @@ const workerCode = `
         return colorNumber;
     }
 
-    self.onmessage = function (e) { // {imageData: ImageData, palette: [r, g, b][],  y_division_last: number, x_division_last: number}
+    self.onmessage = function (e) { // {imageData: ImageData, palette: [r, g, b][]}
         const pixelData = e.data.imageData.data
         const result = []
-        for (let y = 0; y < e.data.y_division_last; y++) {
-            for (let x = 0; x < e.data.x_division_last; x++) {
+        for (let y = 0; y < e.data.imageData.height; y++) {
+            for (let x = 0; x < e.data.imageData.width; x++) {
                 const pixelIndex = (y * e.data.imageData.width + x) * 4;
                 const r = pixelData[pixelIndex];
                 const g = pixelData[pixelIndex + 1];
@@ -82,7 +63,7 @@ const workerCode = `
 const blob = new Blob([workerCode], { type: 'application/javascript' });
 const blobUrl = URL.createObjectURL(blob);
 
-export async function imageData2array(imageData: ImageData, thread_amount: number, palette: [r: number, g: number, b: number][], dither?: string): Promise<{ x: number; y: number; color: number; }[]> { // dither options: FloydSteinberg, FalseFloydSteinberg, Stucki, Atkinson, Jarvis, Burkes, Sierra, TwoSierra, SierraLite
+export async function imageData2array(imageData: ImageData, palette: [r: number, g: number, b: number][], dither?: string): Promise<{ x: number; y: number; color: number; }[]> { // dither options: FloydSteinberg, FalseFloydSteinberg, Stucki, Atkinson, Jarvis, Burkes, Sierra, TwoSierra, SierraLite
     const t0 = performance.now();
     const processing_toast = Toastify ({
         text: `Processing image...`,
@@ -98,31 +79,13 @@ export async function imageData2array(imageData: ImageData, thread_amount: numbe
         quant.reduce(imageData, 1, dither)
     }
     const final_result = await new Promise<{ x: number; y: number; color: number; }[]>(resolve => {
-        const workers_global_result: { x: number; y: number; color: number; }[] = [];
-        let workers = 0;
-        let workers_done = 0;
-        for (let divison_y = 0; divison_y < imageData.height; divison_y += (imageData.height / thread_amount)) {
-            const y_division_last = divison_y + (imageData.height / thread_amount);
-            for (let division_x = 0; division_x < imageData.width; division_x += (imageData.width / thread_amount)) {
-                const x_division_last = division_x + (imageData.width / thread_amount);
-                const worker = new Worker(blobUrl);
-                workers += 1;
-                worker.postMessage({ imageData: imageData, palette: palette, y_division_last: y_division_last, x_division_last: x_division_last });
-                worker.onmessage = function (e) {
-                    const worker_pixels = e.data;
-                    worker.terminate();
-                    workers_done += 1;
-                    console.log(workers_done);
-                    worker_pixels.forEach((pixel: { x: number; y: number; color: number; }) => {
-                        workers_global_result.push(pixel);
-                    });
-                    if (workers == workers_done) {
-                        console.log(`ImageData processed in ${performance.now() - t0} ms`);
-                        resolve(workers_global_result);
-                    }
-                };
-            }
-        }
+        const worker = new Worker(blobUrl);
+        var result = []
+        worker.postMessage({ imageData: imageData, palette: palette });
+        worker.onmessage = function (e) {
+            worker.terminate();
+            resolve(e.data);
+        };
     });
     processing_toast.hideToast();
     Toastify ({
@@ -148,7 +111,7 @@ export async function botImage(x: number, y: number, image: File) {
     const seven = window.seven;
     const bitmap = await createImageBitmap(image);
     let image_data = await imageBitmap2imageData(bitmap);
-    let processed = await imageData2array(image_data, 1, Canvas.instance.colors, window.seven.dither)
+    let processed = await imageData2array(image_data, Canvas.instance.colors, window.seven.dither)
     previewCanvasImage(x, y, image);
     processed = await sort(processed, seven.order)
     processed.forEach((pixel: { x: number; y: number; color: number; }) => {
