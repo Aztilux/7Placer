be08b2dd2e18493006d125f5e8a8f8dab24b905e
Azti <aztilux@gmail.com>
2025-05-12 20:08:56 +0200

    (modify) Remove 0vc4's drop and made key b set image x and y.

--- src/modules/defaultModules/defaultKeys.ts
+++ src/modules/defaultModules/defaultKeys.ts
@@ -1,11 +1,11 @@
-import { createDropArea } from "../../GUI/drop";
 import getClientMouse from "../util/getClientMouse";
 import Queue from "./Queue";
 import { BotSquare } from "./SquareMaker";
 
 var coord1: { x: any; y: any; } = null
-$(document).on('keyup', function(event) {
-    if($(':input[type="text"]').is(':focus')) return //; prevent with chat open
+$(document).on('keydown', function(event) {
+    if($(':input[type="text"]').is(':focus')) return
+    let x: number, y: number, color: number
 
     switch (event.which) {
         case (87):
@@ -13,15 +13,15 @@ $(document).on('keyup', function(event) {
           Queue.stop()
           break
         case (66):
-          if (!event.altKey) return
-          createDropArea()
-          break
+          [x, y, color] = getClientMouse();
+          $("#number_input_X").val(x)
+          $("#number_input_Y").val(y)
+          break;
         case 88:
-          const [x, y, color] = getClientMouse()
-          if (coord1 == null) { coord1 = {x: x, y: y}; return; }
-          BotSquare(coord1.x, coord1.y, x, y, color)
-          coord1 = null
-          break
-        // add more
+          [x, y, color] = getClientMouse();
+          if (coord1 == null) { coord1 = {x: x, y: y}; return; };
+          BotSquare(coord1.x, coord1.y, x, y, color);
+          coord1 = null;
+          break;
     }
 });
